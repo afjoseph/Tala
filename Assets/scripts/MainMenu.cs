4c56b0323d98416e7345e74290dfea3bc2f394b6
afjoseph <abdullahfjoseph@gmail.com>
2017-12-02 14:09:00 +0100

    Add about animation

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -3,34 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
+public class MainMenu : MonoBehaviour {
     private Animator animator;
     public string firstLevel = "puzzle-1";
 
-    void Start()
-    {
-        animator = GetComponent<Animator>();
+    void Start () {
+        animator = GetComponent<Animator> ();
+    }
+
+    void Update () {
+        if (Input.GetKeyDown (KeyCode.Escape)) {
+            onBackPressed_hideAbout ();
+        }
     }
 
     // Use this for initialization
-    void onAnimationCompleted_fade()
-    {
-        animator.SetTrigger("doMove");
+    void onAnimationCompleted_fade () {
+        animator.SetTrigger ("doMove");
     }
 
     // Update is called once per frame
-    void onAnimationCompleted_move()
-    {
-        animator.SetTrigger("doSplit");
+    void onAnimationCompleted_move () {
+        animator.SetTrigger ("doSplit");
     }
 
-    void onAnimationCompleted_split()
-    {
-        animator.SetTrigger("doRevealText");
+    void onAnimationCompleted_split () {
+        animator.SetTrigger ("doRevealText");
     }
 
-    public void onClick_startGame() {
-        SceneManager.LoadScene(firstLevel);
+    public void onClick_startGame () {
+        SceneManager.LoadScene (firstLevel);
+    }
+
+    public void onClick_displayAbout () {
+        animator.SetBool ("displayAbout", true);
+    }
+
+    public void onBackPressed_hideAbout () {
+        animator.SetBool ("displayAbout", false);
     }
 }
